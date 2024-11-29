a8c09d8047abbc1a1655239a61a68e6735633983
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-29 16:13:59 +0100

    add a hook to take the id of the button

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react'
 import { languages } from '../assets/languages'
 
 export function Main(){
-    return(
+    const [value, setValue] = useState(null)
+
+        return(
         <div className='container'>
-            <div className="row">{
+            <div className="row mt-5">{
+
             languages.map((language)=>{
                 return(
-                    <div className="col" key={language.id}>
+                    <div className="col" onClick={()=>{setValue(language.id)}} key={language.id}>
                         <button type="button" className="btn btn-primary">{language.title}</button>
                     </div>               
-                )
-                
+                )              
             })
             }</div>
+            <div className="row mt-5">
+                <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">asf</h5>
+                    <p className="card-text">fasdfgasdgsadgasdgasdf</p>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
