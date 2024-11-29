ad8844057e845788e587a52cf8f245768c46b716
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-29 15:44:23 +0100

    create header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return(
+        <header className="bg-secondary bg-gradient">
+            <h1 className="ps-4">Learn Web Development</h1>
+        </header>
+    )
+}
