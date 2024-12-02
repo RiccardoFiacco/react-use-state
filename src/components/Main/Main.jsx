eb5f39d60898f1e48b13d229990d382e708d3d6e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-12-02 10:31:47 +0100

    add changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
-import { languages } from "../assets/languages";
+import { languages } from "../../assets/languages.js";
 import { Button } from "./Button/Button.jsx";
 import { Card } from "./Card/Card.jsx";
+
 export function Main() {
   console.log(languages);
   const [value, setValue] = useState(null);// setto a null per non impostare automaticamente una card
-  let lenguageClicked = languages[value];
+  let lenguageClicked = languages[value]; //cosi andiamo a prendere l'elemento cliccato dal nostro array di oggetti
+
+  function clickHandle(valueId){
+    setValue(valueId - 1); 
+  }
+
 
   return (
     <div className="container">
       <div className="row mt-5">
-        {languages.map((language) => {
+        {languages.map((language,i) => {
           return (
             <div className="col" key={language.id}>
               <Button 
-               callback={() => setValue(language.id - 1)} //passo una callback che poi riverra eseguita qui
+               callback={() => clickHandle(language.id)} //passo una callback che poi riverra eseguita qui
                title={language.title} 
+               //bgColor = {i=== lenguageClicked ? style.active : ''}
               />
             </div>
           );
